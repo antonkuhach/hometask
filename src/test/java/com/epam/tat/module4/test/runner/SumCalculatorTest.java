45fddb0efd599aa1d9fe094d80257459426f147b
Anton Kuhach <anton_kuhach@epam.com>
2017-08-30 17:08:36 +0300

    Modify SumCalculatorTest tests

--- src/test/java/com/epam/tat/module4/test/runner/SumCalculatorTest.java
+++ src/test/java/com/epam/tat/module4/test/runner/SumCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.epam.tat.module4.test.runner;
 
 import com.epam.tat.module4.Calculator;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,17 +14,23 @@ import static org.junit.Assert.assertEquals;
 public class SumCalculatorTest {
     Calculator calculator;
 
-    @BeforeClass
-    public void beforeSumCalculatorTest() {
+    @Before
+    public void setup() {
         calculator = new Calculator();
     }
 
     @Test
     public void SumTest_DoubleOverflow(){
-        double actual;
         double expected;
+        double actual;
 
-        expected = -1E-323;
-        assertEquals(expected, calculator.sum(-3.142E-320, 3.141E-320), 0.0);
+        expected = 1.5E-323;
+        actual = calculator.sum(-3.142E-320, 3.143E-320);
+        assertEquals(expected, actual, 0.000001);
+    }
+
+    @After
+    public void teardown() {
+        calculator = null;
     }
 }
